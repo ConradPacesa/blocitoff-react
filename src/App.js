a8c60925e67f399afca6809e8f60f78d9c1d876e
Conrad Pacesa <conradpacesa@gmail.com>
2017-12-30 12:24:54 -0500

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Signup from './components/Signup';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/sign-up'>Sign up</Link>
+          </nav>
+          <h1>Blocitoff</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/sign-up" component={Signup} />
+        </main>
       </div>
     );
   }
---
+++ src/components/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Signup = () => (
+  <section className="signup">
+    Sign up page will go here. 
+  </section>
+);
+
+export default Signup;
